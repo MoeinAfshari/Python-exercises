425029d09eda581babcf09f935e495716e623db5
MoeinAfshari <afsharimoein79@gmail.com>
2026-02-02 18:31:09 +0330

    Added geeksforgeeks solution as comment to word guessing game

--- BeginnerProjects/2.py
+++ BeginnerProjects/2.py
@@ -39,3 +39,35 @@ while turns > turn:
 # if the turns finished and the user didn't guess, gets a message 
 if turn == 12 and f == False:
     print("You Loose!")
+
+################################
+# geeksforgeeks solution
+#import random
+#name = input("What is your name? ")
+#print("Good Luck ! ", name)
+#words = ['rainbow', 'computer', 'python', 'science', 'programming', 'geeks']
+#word = random.choice(words)
+#print("Guess the characters")
+#guesses = ''
+#turns = 12
+#while turns > 0:
+#    failed = 0
+#    for char in word:
+#        if char in guesses:
+#            print(char, end=" ")
+#        else:
+#            print("_")
+#            failed += 1
+#    if failed == 0:
+#        print("You Win")
+#        print("The word is: ", word)
+#        break
+#    print()
+#    guess = input("guess a character: ")
+#    guesses += guess
+#    if guess not in word:
+#        turns -= 1
+#        print("Wrong")
+#        print("You have ", + turns, 'more guesses')
+#        if turns == 0:
+#            print("You Loose")
